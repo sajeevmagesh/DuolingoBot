e2b9ee11e3d96c0ed00b42f4556ac622f646ff45
Manujkant <45746374+ManujKant@users.noreply.github.com>
2021-02-06 11:36:14 -0800

    translation added

--- main.py
+++ main.py
@@ -43,3 +43,23 @@ try:
 except:
     pass
 
+def translate(question, language):
+
+    PATH = "./chromedriver"
+
+    driver = webdriver.Chrome(PATH)
+
+    if language == "english":
+        driver.get(f"https://translate.google.com/?sl=auto&tl=en&text={question}&op=translate")
+
+    elif language == "spanish":
+        driver.get(f"https://translate.google.com/?sl=auto&tl=es&text={question}&op=translate")
+
+    else:
+        return None
+    
+    time.sleep(5)
+
+    translation = driver.find_element_by_xpath("/html/body/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[2]/div[5]/div/div[1]/span[1]/span/span")
+
+    return translation.text.lower()

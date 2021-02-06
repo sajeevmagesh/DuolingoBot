ab7bd7ac784b2414508bafc207d5625c170936d2
Manujkant <45746374+ManujKant@users.noreply.github.com>
2021-02-05 19:21:44 -0800

    lol

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
-PATH = "/usr/local/bin/chromedriver"
+username = "Test123"
+password = "Test123"
+
+PATH = "chromedriver"
 
 driver = webdriver.Chrome(PATH)
 
-driver.get("https://www.google.com/")
+driver.get("https://www.duolingo.com/log-in?isLoggingIn=true")
+
+time.sleep(10)
+
+username_and_password = driver.find_element_by_class_name("_3MNft fs-exclude")
+
+for i in username_and_password:
+    i.send_keys("Test123")
+
+
+
